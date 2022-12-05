9843f7dee8915a80dc3208f67f92516dfc15bf7c
Maciek <maciek.klonicki@gmail.com>
2022-12-05 10:25:09 +0100

    first commit

---
+++ src/main/java/org/maciejklonicki/simplewebapp/SimpleWebAppApplication.java
@@ -0,0 +1,13 @@
+package org.maciejklonicki.simplewebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleWebAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleWebAppApplication.class, args);
+    }
+
+}
