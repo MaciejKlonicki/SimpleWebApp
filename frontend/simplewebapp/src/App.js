be6ab9c314008a9d29b3f029d502785cc2d945df
Maciek <maciek.klonicki@gmail.com>
2022-12-07 14:36:43 +0100

    Add another component WhatToDo
    I added component whatToDo to show table and desc, title etc.

--- frontend/simplewebapp/src/App.js
+++ frontend/simplewebapp/src/App.js
@@ -1,12 +1,14 @@
 import React, {Component} from 'react';
 import './App.css';
 import HeaderComp  from './components/HeaderComp';
+import WhatToDo from './components/WhatToDo';
 
 class App extends Component {
     render() {
         return (
             <div className="App">
                 <HeaderComp />
+                <WhatToDo />
             </div>
         );
     }
